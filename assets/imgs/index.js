76f96f396089545ea3b65dd66b934981985584a3
brayangamezt <77857832+brayangamezt@users.noreply.github.com>
2025-01-12 20:56:26 -0700

    Updates

---
+++ assets/imgs/index.js
@@ -0,0 +1,29 @@
+import LogoHarrtPotter  from './logo-hp.png';
+import Single from './single.png';
+import Teams from './teams.png';
+import TresMagos from './tresMagos.png';
+import Adivina from './adivina.jpg';
+import Equipos from './equipos.jpg';
+import Tribia from './tribia.jpg';
+import Train from './horizons_train.png';
+import Gryffindor from './gryffindor.png';
+import Hufflepuff from './hufflepuff.png';
+import Ravenclaw from './ravenclaw.png';
+import Slytherin from './slytherin.png';
+
+
+
+export {
+    LogoHarrtPotter,
+    Single,
+    Teams,
+    TresMagos,
+    Adivina,
+    Equipos,
+    Tribia,
+    Train,
+    Gryffindor,
+    Hufflepuff,
+    Ravenclaw,
+    Slytherin
+}
