d8c1a91f63ce6db1bc2ec59be481b9b8501cb48f
brayangamezt <77857832+brayangamezt@users.noreply.github.com>
2026-02-21 23:35:47 -0700

    Update Character.jsx

--- src/pages/singles/Character.jsx
+++ src/pages/singles/Character.jsx
@@ -10,7 +10,7 @@ export const Character = ({players, setPlayers,setObteined, obteined, turn,setTu
 
     const [startGame, setStartGame] = useState(false);
     const [next, setNext] = useState( false );
-    const [seconds, setSeconds] = useState(25);
+    const [seconds, setSeconds] = useState(40);
     const [cards, setCards] = useState( cardsCharacters );
     const [selectedCard, setSelectedCard] = useState(null);
     const [deletedNumber, setDeletedNumber] = useState([]);
